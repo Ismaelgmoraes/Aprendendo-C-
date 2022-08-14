1a8a53e17c94ddbb49fd8aad96a60c236358b382
Ismael Gomes de Moraes <ismaelgmoraes@gmail.com>
2022-08-13 21:43:38 -0300

    insersao main e algumas funçoes

--- src/Fila.cpp
+++ src/Fila.cpp
@@ -2,6 +2,8 @@
 #include "Fila.h"
 
 using namespace std;
+//const int max_item = 100;
+//typedef int TipoItem
 
         Fila::Fila()
         {
@@ -15,21 +17,41 @@ using namespace std;
         }
         bool Fila::estavazio()
         {
-
+            return (primeiro == ultimo);
         }
         bool Fila::estacheio()
         {
-
+            return ((ultimo - primeiro) == max_itens);
         }
         void Fila::inserir(TipoItem item) //push
         {
-
+            if (estacheio())
+	 	{
+	 		cout << "A fila está cheia\n";
+	 		cout <<"Não foi possivel inserir o item\n";
+	 	}else{
+	 		estrutura [ultimo % max_itens] = item;
+	 		ultimo++;
+	 	}
         }
         TipoItem Fila::remover() //pop
         {
-
+            if(estavazio())
+ 	 {
+	 	cout << "A pilha está vazia\n";
+	 	cout <<"nenhum elemento foi removido\n";
+	 	return 0;
+	 } else{
+	 	primeiro++;
+	 	return estrutura[(primeiro-1)% max_itens];
+	 }
         }
         void Fila::imprimir()
         {
-
+             cout <<"Fila: [ ";
+	 	 for(int i = primeiro; i < ultimo; i++)
+	 	 {
+	 	 	cout << estrutura[ i% max_itens] << " ";
+	 	 }
+	 	  cout << "]\n\n";
         }
